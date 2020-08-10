c87bcce4ff33f221614649ee17736c792444b615
carolcarvalhosa <ccscarvalhosa@gmail.com>
2020-08-10 16:08:13 -0300

    Connecting API and finishing front-end

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,32 +1,53 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = (props) => {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        });
+    }
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png" alt="" />
+                <img src={teacher.avatar} alt="" />
                 <div>
-                    <strong>Diego Fernandes</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>{teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    target="_blank"
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    onClick={createNewConnection}
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
